1a61bf0c542ac22471f82481ef5d69f095f61b89
Tim Grillmeier <tlgrillmeier@gmail.com>
2025-07-24 20:16:50 +1000

    Capturing query terms and pagination index with pretty URLs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/page/:page",
+        destination: "/?page=:page",
+      },
+      {
+        source: "/query/:query",
+        destination: "/?query=:query",
+      },
+      {
+        source: "/query/:query/page/:page",
+        destination: "/?query=:query&page=:page",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home({ searchParams }: { searchParams: Promise<{ query: string; page: string }>; }) {
+  const { query = '', page = '1'} = await searchParams
+  const paginationIndex = parseInt(page) // 1 it not provided
+  const paginationCount = 6 // assuming 6 per page for now
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>Martin's Movies</h1>
+        <p>Params test</p>
+        <p>Query: "{query}"</p>
+        <p>Page: "{paginationIndex}"</p>
       </main>
     </div>
   );
