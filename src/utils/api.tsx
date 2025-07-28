99f41c77a50b5b52dfba42410e263885ba5cc0a7
Tim Grillmeier <tlgrillmeier@gmail.com>
2025-07-28 22:15:35 +1000

    Added getMovieGenres function to call TMDB API for genres and IDs

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,3 +1,30 @@
+export async function getMovieGenres() {
+    // Prepare API call
+    const url = new URL(`${API_ROOT}/3/genre/movie/list`);
+
+    const options: RequestInit = {
+        method: 'GET',
+        cache: 'no-cache',
+        headers:  {
+            Accept: 'application/json',
+            Authorization: `Bearer ${ACCESS_TOKEN}`
+        }
+    };
+
+    // Make API call
+    const res = await fetch(url.toString(), options);
+    if (!res.ok) {
+        console.error(`Issue fetching movie genres: ${res.status} ${res.statusText} - ${url.toString()}`);
+        try {
+            const errorBody = await res.text();
+            console.error('Response body:', errorBody);
+        } catch (e) {
+            console.error(e);
+        }
+        return null;
+    }
+    return res.json();
+}
 const API_ROOT = process.env.TMDB_API_ROOT!
 const ACCESS_TOKEN = process.env.TMDB_API_ACCESS_TOKEN!
 const API_KEY = process.env.TMDB_API_KEY!
@@ -35,7 +62,7 @@ export async function searchMovies({
             const errorBody = await res.text();
             console.error('Response body:', errorBody);
         } catch (e) {
-            // ignore
+           console.error(e);
         }
         return null;
     }
@@ -71,7 +98,7 @@ export async function getPopularMovies({
             const errorBody = await res.text();
             console.error('Response body:', errorBody);
         } catch (e) {
-            // ignore
+            console.error(e);
         }
         return null;
     }
